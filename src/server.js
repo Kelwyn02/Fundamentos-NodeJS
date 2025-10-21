d1ecd369c4658a633125d8635f5518b7e1481da7
Kelwyn02 <kelwyn12345@hotmail.com>
2025-10-21 20:31:51 -0300

    code fixes

--- src/server.js
+++ src/server.js
@@ -1,41 +1,41 @@
-import http from 'node:http'
+import http from "node:http";
 
-const users = []
+const users = [];
 
 const server = http.createServer(async (req, res) => {
-  const { method, url } = req
+  const { method, url } = req;
 
-  const buffers = []
+  const buffers = [];
 
   for await (const chunk of req) {
-    buffers.push(chunk)
+    buffers.push(chunk);
   }
 
   try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
   } catch {
-    req.body = null
+    req.body = null;
   }
 
-  if (method === 'GET' && url === '/users') {
+  if (method === "GET" && url === "/users") {
     return res
-      .setHeader('Content-type', 'application/json')
-      .end(JSON.stringify(users))
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
   }
 
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = req.body
+  if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
 
     users.push({
       id: 1,
       name,
       email,
-    })
+    });
 
-    return res.writeHead(201).end()
+    return res.writeHead(201).end();
   }
 
-  return res.writeHead(404).end()
-})
+  return res.writeHead(404).end();
+});
 
-server.listen(3333)
+server.listen(3333);
